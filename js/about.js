5594a0d872b383d03ca5bd7af78b9bdc5114978a
MIKE <mikhail.khakalo@gmail.com>
2022-01-17 00:07:17 +0300

    about added

---
+++ js/about.js
@@ -0,0 +1,49 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 1
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 1000}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides-fade");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
