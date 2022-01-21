8da5ea347f00b2f0b23efdf0110f1fb656d752b1
MIKE <mikhail.khakalo@gmail.com>
2022-01-21 17:46:59 +0300

    add modal, delete trash

--- js/index.js
+++ js/index.js
@@ -1,32 +1,14 @@
-/* Open when someone clicks on the span element */
 function openNav() {
     document.getElementById('myNav').style.width = "100%";
     document.getElementById('slogan').hidden = true;
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById('myNav').style.width = "0%";
     document.getElementById('slogan').hidden = false;
 }
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById('slide').style.top = "0";
-    } else {
-        document.getElementById('slide').style.top = "-300px";
-    }
-    prevScrollpos = currentScrollPos;
-}
-
-
-//Get the button:
 mybutton = document.getElementById("myTop");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -37,21 +19,18 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
